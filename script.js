658743ed905ad091b7a5bf46be5f29fd343b63ce
Meitashi <138116123+Meitashi@users.noreply.github.com>
2024-02-06 14:38:58 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector("button");
+const text = document.querySelector(".text");
+
+btn.addEventListener("click", function(){
+    const query = fetch(`https://catfact.ninja/fact`);
+    query
+    .then(data => data.json())
+    .then(quote => generateQuote(quote))
+    console.log(query)
+});
+
+function generateQuote(quoteCode){
+    text.innerHTML = "";
+    text.innerHTML = quoteCode.fact;
+};
